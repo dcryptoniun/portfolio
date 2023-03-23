198d3bcb1e66ff66604a7cc56eb6907dd39a4c55
dcryptoniun <dcryptoniun@gmail.com>
2023-03-23 15:28:50 +0530

    v1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<>
+  <ThemeProvider attribute="class">
+  <Navbar/>
+  <Component {...pageProps} />
+  {/* <Footer/> */}
+  </ThemeProvider>
+  </> 
 }
